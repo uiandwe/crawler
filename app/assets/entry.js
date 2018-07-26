86c0a53b623f9e8afb1466f56fd924f5bb7918aa
uiandwe <uiandwe@gmail.com>
2018-07-27 00:09:17 +0900

    1:  캐러셀 삭제

--- app/assets/entry.js
+++ app/assets/entry.js
@@ -6,5 +6,8 @@ require('!style!css!./css/main.scss');
 
 var hello = require('./js/hello');
 var world = require('./js/world');
+var main = require('./js/main');
+
+// import 'expose-loader?$!expose-loader?jQuery!jquery'
 
 document.write(hello + ', ' + world + '!');
